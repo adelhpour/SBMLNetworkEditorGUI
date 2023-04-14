0a5e5a46d773189028a3cc7c6474a32ef6b70730
adelhpour <>
2023-04-14 16:05:52 -0700

    curve menu is added alongside bounding box menu for reactions

--- src/libsbml_ne_reaction.cpp
+++ src/libsbml_ne_reaction.cpp
@@ -3,6 +3,8 @@
 #include "libsbml_ne_feature_menu.h"
 #include <QGridLayout>
 
+using namespace LIBSBML_NETWORKEDITOR_CPP_NAMESPACE;
+
 MyReaction::MyReaction(GraphicalObject* graphicalObject, Style* style, const qreal& graphicsItemZValue) : MyNetworkElementBase(graphicalObject, style, graphicsItemZValue) {
     
 }
@@ -42,18 +44,33 @@ QWidget* MyReaction::elementFeatureMenu() {
     contentLayout->addItem(new MySpacerItem(0, 20), contentLayout->rowCount(), 0, 1, 2);
     
     MyTreeView* featureMenuTree = new MyTreeView(elementFeatureMenu);
-    
-    // bounding box
-    QWidget* _boundingBoxMenu = new MyBoundingBoxMenu(_graphicalObject);
-    connect(_boundingBoxMenu, SIGNAL(isUpdated()), this, SIGNAL(isUpdated()));
-    connect(_boundingBoxMenu, SIGNAL(isUpdated()), this, SLOT(updateGraphicsItem()));
-    featureMenuTree->addBranchWidget(_boundingBoxMenu, "BoundingBox");
-    
-    // geometric shape
-    QWidget* _geometricShapeMenu = new MyGeometricShapesMenu(_style->getGroup());
-    connect(_geometricShapeMenu, SIGNAL(isUpdated()), this, SIGNAL(isUpdated()));
-    connect(_geometricShapeMenu, SIGNAL(isUpdated()), this, SLOT(updateGraphicsItem()));
-    featureMenuTree->addBranchWidget(_geometricShapeMenu, "Geometric Shapes");
+
+    if (isSetCurve(_graphicalObject)) {
+        // stroke
+        QWidget* _strokeMenu = new MyStrokeMenu(_style->getGroup());
+        connect(_strokeMenu, SIGNAL(isUpdated()), this, SIGNAL(isUpdated()));
+        connect(_strokeMenu, SIGNAL(isUpdated()), this, SLOT(updateGraphicsItem()));
+        featureMenuTree->addBranchWidget(_strokeMenu, "Stroke");
+
+        // curve
+        QWidget* _curveMenu = new MyCurveMenu(_graphicalObject);
+        connect(_curveMenu, SIGNAL(isUpdated()), this, SIGNAL(isUpdated()));
+        connect(_curveMenu, SIGNAL(isUpdated()), this, SLOT(updateGraphicsItem()));
+        featureMenuTree->addBranchWidget(_curveMenu, "Curve");
+    }
+    else {
+        // bounding box
+        QWidget* _boundingBoxMenu = new MyBoundingBoxMenu(_graphicalObject);
+        connect(_boundingBoxMenu, SIGNAL(isUpdated()), this, SIGNAL(isUpdated()));
+        connect(_boundingBoxMenu, SIGNAL(isUpdated()), this, SLOT(updateGraphicsItem()));
+        featureMenuTree->addBranchWidget(_boundingBoxMenu, "BoundingBox");
+
+        // geometric shape
+        QWidget* _geometricShapeMenu = new MyGeometricShapesMenu(_style->getGroup());
+        connect(_geometricShapeMenu, SIGNAL(isUpdated()), this, SIGNAL(isUpdated()));
+        connect(_geometricShapeMenu, SIGNAL(isUpdated()), this, SLOT(updateGraphicsItem()));
+        featureMenuTree->addBranchWidget(_geometricShapeMenu, "Geometric Shapes");
+    }
     
     contentLayout->addWidget(featureMenuTree, contentLayout->rowCount(), 0, 1, 2);
     
